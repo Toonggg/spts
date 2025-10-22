26a283278bb5e1b0401e4f63719c87ed61706dc6
Patrick Adams <patrick_aust@hotmail.com>
2025-10-22 12:51:34 +0200

    added conda script

---
+++ conda-script.sh
@@ -0,0 +1,11 @@
+
+
+conda create -n spts python==3.8
+conda install numpy -y
+conda install cython -y
+conda install h5py -y
+conda install scipy -y
+conda install -c conda-forge expiringdict -y
+conda install -c conda-forge olefile -y
+conda install -c conda-forge typing -y
+
